1560bbf7ef92f49cff2c8dd7424c3bf2fcd6e2ca
Jiaan Fang <jiaanf@gmail.com>
2024-01-30 19:16:21 +0800

    update highlighter.scm

--- Sources/TreeSitterResource/LanguageResources/yaml/highlights.scm
+++ Sources/TreeSitterResource/LanguageResources/yaml/highlights.scm
@@ -1,16 +1,29 @@
-(pair
-  key: (_) @string.special.key)
+;; keys
+(block_mapping_pair
+ key: (flow_node [(double_quote_scalar) (single_quote_scalar)] @variable))
+(block_mapping_pair
+ key: (flow_node (plain_scalar (string_scalar) @variable)))
 
-(string) @string
+;; keys within inline {} blocks
+(flow_mapping
+ (_ key: (flow_node [(double_quote_scalar) (single_quote_scalar)] @variable)))
+(flow_mapping
+ (_ key: (flow_node (plain_scalar (string_scalar) @variable))))
 
-(number) @number
+["[" "]" "{" "}"] @punctuation.bracket
+["," "-" ":" "?" ">" "|"] @punctuation.delimiter
+["*" "&" "---" "..."] @punctuation.special
 
-[
-  (null)
-  (true)
-  (false)
-] @constant.builtin
-
+[(null_scalar) (boolean_scalar)] @constant.builtin
+[(integer_scalar) (float_scalar)] @number
+[(double_quote_scalar) (single_quote_scalar) (block_scalar)] @string
 (escape_sequence) @escape
 
 (comment) @comment
+[(anchor_name) (alias_name)] @function
+(yaml_directive) @type
+
+(tag) @type
+(tag_handle) @type
+(tag_prefix) @string
+(tag_directive) @property
